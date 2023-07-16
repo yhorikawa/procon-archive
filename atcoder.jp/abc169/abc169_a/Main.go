6828b8977402c4b204dd3c69fc27efb2b0de8427
Horikawa <75514868+yhorikawa@users.noreply.github.com>
2023-07-16 07:37:41 +0000

    [AC] abc169 abc169_a

---
+++ atcoder.jp/abc169/abc169_a/Main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	fmt.Println(a * b)
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
