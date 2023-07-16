6ee84d1bdaa3350b79e565078fee0bc6212d22cf
Horikawa <75514868+yhorikawa@users.noreply.github.com>
2023-07-16 10:30:26 +0000

    [AC] abc139 abc139_a

---
+++ atcoder.jp/abc139/abc139_a/Main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer out.Flush()
+
+	sc.Split(bufio.ScanWords) // スペース区切りの設定
+	s := nextString()
+	t := nextString()
+	j := strings.Split(s, "")
+	v := strings.Split(t, "")
+	count := 0
+	for i, str := range j {
+		if str == v[i] {
+			count++
+		}
+	}
+	fmt.Fprintln(out, count)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
