1411803635e1183f01175d1364e77499bd8270d3
Horikawa <75514868+yhorikawa@users.noreply.github.com>
2023-07-17 01:58:27 +0000

    [AC] abc088 abc088_a

---
+++ atcoder.jp/abc088/abc088_a/Main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer out.Flush()
+
+	sc.Split(bufio.ScanWords) // スペース区切りの設定
+	N := nextInt()
+	A := nextInt()
+
+	mod := N % 500
+	if A >= mod {
+		fmt.Fprintln(out, "Yes")
+		return
+	}
+
+	fmt.Fprintln(out, "No")
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
