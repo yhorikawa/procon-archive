19c6e5a1e8d73a5367c5e26cd505d2491d432146
Horikawa <75514868+yhorikawa@users.noreply.github.com>
2024-01-13 23:14:59 +0900

    [AC] abc112 abc112_b

---
+++ atcoder.jp/abc112/abc112_b/Main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(r io.Reader, w io.Writer) {
+	var sc = bufio.NewScanner(r)
+	var out = bufio.NewWriter(w)
+
+	defer out.Flush()
+
+	sc.Split(bufio.ScanWords) // スペース区切りの設定
+	N := nextInt(sc)
+	T := nextInt(sc)
+
+	result := 10000
+	for i := 0; i < N; i++ {
+		c := nextInt(sc)
+		t := nextInt(sc)
+		if t <= T {
+			result = min(result, c)
+		}
+	}
+
+	if result == 10000 {
+		fmt.Fprintln(out, "TLE")
+		return
+	}
+
+	fmt.Fprintln(out, result)
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
